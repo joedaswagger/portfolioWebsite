76c21fde5bc90f56a32de53be68e9f39bd265d15
joedaswagger <95245646+joedaswagger@users.noreply.github.com>
2026-02-04 11:42:49 -0500

    Added stars and meteors for dark mode

--- websitePortfolio/src/pages/Home.jsx
+++ websitePortfolio/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -7,7 +8,7 @@ export const Home = () => {
         <ThemeToggle />
 
         {/* Background Effects */}
-        
+        <StarBackground />
 
         {/* Navbar */}
 
